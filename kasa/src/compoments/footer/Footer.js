6d4e88d5cb6f47c82b4c03f7df8886b19f600b7a
blernix <killian.lecrut@gmail.com>
2023-07-25 18:45:05 +0200

    nav & footer

--- kasa/src/compoments/footer/Footer.js
+++ kasa/src/compoments/footer/Footer.js
@@ -1,6 +1,17 @@
+import logo from '../../assets/logonoir.png';
+import '../../compoments/footer/Footer.scss'
+
 function Footer() {
     return (
-   <h1>Footer</h1> )
+        <div className='footer'>
+        <div className='navLogo'>
+        <img src={logo} alt='Kasa' className='kasa-logonoir' />
+        </div>
+        <div className='copyright'>
+            <p>© 2020 Kasa. All rights reserved</p>
+        </div>
+    </div>
+    )
 }
 
 export default Footer
--- kasa/src/compoments/nav/Nav.js
+++ kasa/src/compoments/nav/Nav.js
@@ -1,6 +1,20 @@
-function Nav () {
+import '../nav/Nav.scss'
+import logo from '../../assets/logorouge.png';
+
+function Nav() {
     return (
-        <h1>NavBar</h1>
+        <header className='navigation'>
+            <div className='navLogo'>
+            <img src={logo} alt='Kasa' className='kasa-logo' />
+            </div>
+            <nav className='navbar'>
+                <ul>
+                    <li className="navLink">Accueil</li>
+                    <li className="navLink">A propos</li>
+                </ul>
+            </nav>
+        </header>
     )
 }
+
 export default Nav
